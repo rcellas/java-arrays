cbcf04c66471adf2ae84de9357f926fdea77006f
unknown <crismouta@gmail.com>
2024-11-05 14:05:06 +0100

    java-arrays ejercicio

---
+++ src/Welcome.java
@@ -0,0 +1,16 @@
+public class Welcome {
+    public static void main(String[] args) {
+
+        //Crea un array que contenga 5 nombres de coders y con un bucle 'for-each' imprime: ¡Hola <name>, bienvenida a Java!
+
+
+        //Escribe un bucle 'for' que imprima solo los siguientes números: 10 - 30 - 50 - 70 - 90 del array numbers, sin modificarlo ni creando uno nuevo
+
+
+        //Crea un array que contenga algunos números y usa un bucle 'for-each' e imprime cada número duplicado
+
+
+        //Crea un array de tipo char que contenga las vocales en minúsculas y usa un bucle 'for-each' e imprime cada vocal en mayúsculas
+
+    }
+}
